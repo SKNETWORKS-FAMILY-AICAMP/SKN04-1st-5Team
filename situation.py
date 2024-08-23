51ec6d60f431998d74555f148397140464a24dea
dldkddkd <jisoolee1997@naver.com>
2024-08-23 14:19:29 +0900

    fix: city mapping code added

--- situation.py
+++ situation.py
@@ -9,6 +9,28 @@ from dbconnect import connect_to_postgres
  
 
 def crawl_ev_situation():
+
+    # 도시 코드 매핑 딕셔너리
+    code_mapping = {
+        '서울': 1,
+        '부산': 2,
+        '대구': 3,
+        '인천': 4,
+        '광주': 5,
+        '대전': 6,
+        '울산': 7,
+        '세종': 8,
+        '경기': 9,
+        '강원': 10,
+        '충북': 11,
+        '충남': 12,
+        '전북': 13,
+        '전남': 14,
+        '경북': 15,
+        '경남': 16,
+        '제주': 17
+    }
+
     url = 'https://chargeinfo.ksga.org/front/statistics/evCar'
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
@@ -17,9 +39,23 @@ def crawl_ev_situation():
 
     thead = bs4.select('table.datatable thead th')
     locations = [column.text for column in thead]
+    print(locations[1:-1])
     tr = bs4.select('table.datatable tbody tr')
-
+    
     conn = connect_to_postgres()
+    # if conn != None:
+    #     for city in locations[1:-1]:
+    #         city_name = city
+    #         mapped_code = code_mapping.get(city_name, city_name)
+
+    #         try:    
+    #             cur = conn.cursor()
+    #             cur.execute(f"INSERT INTO city_table (city_code, city_name) VALUES ('{mapped_code}', '{city}');")
+    #             conn.commit() # CREATE    
+    #         except Exception as e:
+    #             print(f"{mapped_code}', '{city}: {e}")
+    #             pass
+
     if conn != None:
         for row in tr:
             data = row.select('td')
@@ -42,11 +78,15 @@ def crawl_ev_situation():
                     # 데이터에서 ,제거하기
                     split_data = split_data.replace(',','')
                     #print(locations[idx], year, split_data)
+                    city_name = locations[idx]
+                    mapped_code = code_mapping.get(city_name, city_name)
 
                     try:    
                         cur = conn.cursor()
-                        # cur.execute(f"INSERT INTO situation_table (situation_city, situation_year, situation_amount) VALUES ('{locations[idx]}', '{year}', '{split_data}');")
-                        conn.commit() # CREATE    
+                        cur.execute(f"INSERT INTO situation_table (situation_city, situation_year, situation_amount) VALUES ('{mapped_code}', '{year}', '{split_data}');")
+                        conn.commit() # CREATE
                     except Exception as e:
-                        print(f"Cannot insert values into the database: {e}")
+                        print(f"{mapped_code}, {year}, {split_data}: {e}")
                         pass
+
+                    
